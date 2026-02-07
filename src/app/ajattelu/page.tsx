f62e6de5b9a6d9843c73afa1a8ac50c21df23703
JoonasHuhta <jvkeranen@gmail.com>
2026-02-07 20:12:40 +0200

    fix(ajattelu): properly structure divs to show page content

--- src/app/ajattelu/page.tsx
+++ src/app/ajattelu/page.tsx
@@ -299,6 +299,8 @@ export default function AjatteluPage() {
                         })()}
                     </div>
                 </div>
+
             </div>
-            );
+        </div>
+    );
 }
